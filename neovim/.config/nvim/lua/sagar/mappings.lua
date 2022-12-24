5d02a35a4e1d31ece70b9b9c9f60fb395b8521ae
sagarpakhrin <sasgarlama826@gmail.com>
2022-12-24 23:22:47 +0545

    clean up

--- neovim/.config/nvim/lua/sagar/mappings.lua
+++ neovim/.config/nvim/lua/sagar/mappings.lua
@@ -45,3 +45,4 @@ vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
 vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
 vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
+vim.keymap.set("n", "<leader>ev", ":vsp ~/.config/nvim/init.lua<cr>")
--- neovim/.config/nvim/lua/sagar/options.lua
+++ neovim/.config/nvim/lua/sagar/options.lua
@@ -23,6 +23,3 @@ vim.opt.isfname:append("@-@")
 vim.opt.updatetime = 50
 
 vim.opt.colorcolumn = "80"
-
-vim.g.mapleader = " "
-
