55cadab40c9f255fc6e127b527fa2a97bb6d86bc
sagarPakhrin <sagarlama826@gmail.com>
2023-01-28 15:59:52 +0545

    refactor: Move formatting to lsp from null-ls

--- neovim/.config/nvim/after/plugin/lsp.lua
+++ neovim/.config/nvim/after/plugin/lsp.lua
@@ -27,13 +27,36 @@
 
 -- LSP settings.
 --  This function gets run when an LSP connects to a particular buffer.
-local on_attach = function(_, bufnr)
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+
+local on_attach = function(client, bufnr)
   -- NOTE: Remember that lua is a real programming language, and as such it is possible
   -- to define small helper and utility functions so you don't have to repeat yourself
   -- many times.
   --
   -- In this case, we create a function that lets us more easily define mappings specific
   -- for LSP related items. It sets the mode, buffer and description for us each time.
+  if client.supports_method("textDocument/formatting") then
+    vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      group = augroup,
+      buffer = bufnr,
+      callback = function()
+        -- this is not working, client.name is null-ls
+        if client.name == "tsserver" then
+          vim.lsp.buf.execute_command({
+            command = "_typescript.organizeImports",
+            arguments = { vim.api.nvim_buf_get_name(0) },
+          })
+        end
+        vim.lsp.buf.format()
+
+      end,
+    })
+  end
+
+
+
   local nmap = function(keys, func, desc)
     if desc then
       desc = 'LSP: ' .. desc
--- neovim/.config/nvim/after/plugin/null-ls.lua
+++ neovim/.config/nvim/after/plugin/null-ls.lua
@@ -2,29 +2,28 @@ local null_ls = require("null-ls")
 
 local builtins = null_ls.builtins
 
-local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+-- local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
-local on_attach = function(client, bufnr)
-  if client.supports_method("textDocument/formatting") then
-    vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-    vim.api.nvim_create_autocmd("BufWritePre", {
-      group = augroup,
-      buffer = bufnr,
-      callback = function()
-
-        -- this is not working, client.name is null-ls
-        -- if client.name == "tsserver" then
-        vim.lsp.buf.execute_command({
-          command = "_typescript.organizeImports",
-          arguments = { vim.api.nvim_buf_get_name(0) },
-        })
-        -- end
-        vim.lsp.buf.format()
-
-      end,
-    })
-  end
-end
+-- local on_attach = function(client, bufnr)
+--   if client.supports_method("textDocument/formatting") then
+--     vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+--     vim.api.nvim_create_autocmd("BufWritePre", {
+--       group = augroup,
+--       buffer = bufnr,
+--       callback = function()
+--         -- this is not working, client.name is null-ls
+--         -- if client.name == "tsserver" then
+--         vim.lsp.buf.execute_command({
+--           command = "_typescript.organizeImports",
+--           arguments = { vim.api.nvim_buf_get_name(0) },
+--         })
+--         -- end
+--         vim.lsp.buf.format()
+--
+--       end,
+--     })
+--   end
+-- end
 
 null_ls.setup({
   debug = false,
@@ -35,5 +34,5 @@ null_ls.setup({
     builtins.completion.spell,
     builtins.code_actions.eslint,
   },
-  on_attach = on_attach
+  -- on_attach = on_attach
 })
