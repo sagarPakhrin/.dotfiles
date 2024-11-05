49ffbc0a01068c190d4ac94a3cfd34489bf5dbfd
sagarPakhrin <sagarlama826@gmail.com>
2024-11-05 11:27:02 +0545

    changes

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -443,7 +443,7 @@ require('lazy').setup {
         --    https://github.com/pmizio/typescript-tools.nvim
         --
         -- But for many setups, the LSP (`tsserver`) will work just fine
-        tsserver = {
+        ts_ls = {
           keys = {
             {
               '<leader>co',
